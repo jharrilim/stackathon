9dd42b22f543745476a01b4f777f8ccb4f12f5e3
Joseph Harrison-Lim <Josephharrisonlim@gmail.com>
2019-05-04 22:43:04 -0400

    Add TypeOrm Module

--- api-v2/src/app.module.ts
+++ api-v2/src/app.module.ts
@@ -1,12 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AnswerController } from './answer/answer.controller';
 import { UserController } from './user/user.controller';
 import { QuestionController } from './question/question.controller';
 
+const OrmModule = TypeOrmModule.forRoot({
+    type: 'postgres',
+    database: process.env.DB_USER || 'admin',
+    username: process.env.DB_USER || 'admin',
+    password: process.env.DB_PASSWORD || 'postgres',
+    port: +(process.env.DB_PORT || 5432),
+    host: process.env.DB_HOST || 'localhost',
+    entities: [`${__dirname}/**/*.entity.ts`],
+    synchronize: process.env.NODE_ENV !== 'production',
+    logger: 'advanced-console',
+    logging: 'all',
+    dropSchema: process.env.NODE_ENV !== 'production',
+    cache: true,
+});
+
 @Module({
-    imports: [],
+    imports: [OrmModule],
     controllers: [AppController, AnswerController, UserController, QuestionController],
     providers: [AppService],
 })
--- api-v2/src/question/question.controller.ts
+++ api-v2/src/question/question.controller.ts
@@ -1,4 +1,6 @@
 import { Controller } from '@nestjs/common';
 
 @Controller('question')
-export class QuestionController {}
+export class QuestionController {
+    
+}
